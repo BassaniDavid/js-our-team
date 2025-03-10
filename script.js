10e5720893a94ccdec6c552161d919619d8c3339
BassaniDavid <davbassan98@gmail.com>
2025-03-10 16:24:48 +0100

    feat: bonus complete

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const teamMembers = [
+let teamMembers = [
   {
     name: "Marco Bianchi",
     role: "Designer",
@@ -39,26 +39,25 @@ const teamMembers = [
 
 // seleziono il container
 const containerElement = document.querySelector('.container')
-createHtmlText (teamMembers)
+createHtmlText(teamMembers)
 
 // funzione per rendere html ready un oggetto
-function createHtmlText (elements) {
+function createHtmlText(elements) {
   let texts = '';
 
   // seleziono ogni oggetto nell array
-  for(let i = 0; i < elements.length; i++){
+  for (let i = 0; i < elements.length; i++) {
 
     // inserisco, tramite la prima funzione, gli elementi dell oggetto nel html precompilato e aggiungo per ogni iterazione il testo ad una variabile
-    texts += createHtmlElement (elements[i])
+    texts += createHtmlElement(elements[i])
   }
-  console.log(texts)
   // inserisco tutto il testo html generato nel container
   containerElement.innerHTML = texts
 }
 
 
 // funzione per creare HTML
-function createHtmlElement (element) {
+function createHtmlElement(element) {
   return `<div class="card">
                 <img src="${element.img}" alt="immagine di ${element.name}">
                 <div class="card-content">
@@ -69,4 +68,41 @@ function createHtmlElement (element) {
           \</div>`
 }
 
-console.log(containerElement.innerHTML)
+
+// bonus
+
+// richiamo il form in js
+const formElement = document.querySelector('form')
+
+// richiamo i campi di input del form
+let name = document.getElementById('name')
+let role = document.getElementById('role')
+let email = document.getElementById('email')
+let img = document.getElementById('img')
+
+// richiamo il submit del form in js
+const buttonElement = document.getElementById('submit')
+
+// previene che la pagina si ricarichi premento submit e che i dati vengano persi
+formElement.addEventListener('submit', (event) => {
+  event.preventDefault()
+})
+
+// // azione all'invio dei dati inseriti dall'utente
+buttonElement.addEventListener('click', () => {
+  console.log('l utente ha inviato il form')
+
+  let userObj = {
+    name: name.value,
+    role: role.value,
+    email: email.value,
+    img: img.value
+  }
+
+  teamMembers.push(userObj) 
+  console.log(teamMembers)
+  createHtmlText(teamMembers)
+  formElement.reset()
+})
+
+
